d3c1fbb12d77376a15711547f610fe08cbcf82e2
utsavisama <82657781+utsavisama@users.noreply.github.com>
2022-02-11 17:56:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 cat = ""
 object = [];
 status = "";
+var r = 0;
+var g = 0;
+var b = 0; 
 
 
 function preload(){
@@ -10,6 +13,9 @@ cat = loadImage("dog_cat.jpg")
 function setup(){
 canvas = createCanvas(600, 400)
 canvas.position(350,200)
+video = createCapture(VIDEO)
+video.size(600, 400)
+video.hide()
 objectDetector = ml5.objectDetector('cocossd', modelloaded)
 document.getElementById("status").innerHTML = "Status : Detecting Objects!!!! "
 }
@@ -17,7 +23,7 @@ document.getElementById("status").innerHTML = "Status : Detecting Objects!!!! "
 function modelloaded(){
     console.log("Your Super, fast model is loaded now!!")
     status = true;
-    objectDetector.detect(cat, gotresluts)
+    objectDetector.detect(video, gotresluts)
 }
 
 function gotresluts(error, results){
@@ -32,15 +38,19 @@ function gotresluts(error, results){
 }
 
 function draw(){
-image(cat,0,0,600,400)
+image(video,0,0,600,400)
+r = random(255);
+g = random(255);
+b = random(255);
 if(status !=""){
     for(var i = 0; i<object.length; i++){
         document.getElementById("status").innerHTML = "Status : OBJECTS DETECTED BY OUR SUPER FAST MODEL! "
-        fill("purple")
+        document.getElementById("status").innerHTML = "NUMBER OF OBJECTS DETECTED ARE :  "+object.length
+        fill(r, g, b)
         percent = floor(object[i].confidence*100)
         text(object[i].label + " " + percent+" % ", object[i].x+15,object[i].y+15)
         noFill()
-        stroke("black")
+        stroke(r, g, b)
         rect(object[i].x, object[i].y, object[i].width, object[i].height   )
 }
 
